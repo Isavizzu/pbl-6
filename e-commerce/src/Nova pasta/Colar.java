b4a47c8da194859f47a859c32a87ae46c7adbcd1
Mafezuda <moreira.mafe@gmail.com>
2024-04-11 14:34:20 -0700

    .

--- e-commerce/src/Nova pasta/Colar.java
+++ e-commerce/src/Nova pasta/Colar.java
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-public class Colar extends Acessorio {
+public class Colar extends Acessorios {
     private String tipoFecho;
 
     public Colar(String material, String cor, String marca, String preco, String forma, String genero, String categoria_acessorio, String revestimento, String tipoFecho) {
@@ -15,21 +14,3 @@ public class Colar extends Acessorio {
         return this.tipoFecho;
     }
 }
-=======
-public class Colar extends Acessorio {
-    private String tipoFecho;
-
-    public Colar(String material, String cor, String marca, String preco, String forma, String genero, String categoria_acessorio, String revestimento, String tipoFecho) {
-        super(material, cor, marca, preco, forma, genero, categoria_acessorio, revestimento);
-        this.tipoFecho = tipoFecho;
-    }
-
-    public void setTipoFecho(String tipoFecho) {
-        this.tipoFecho = tipoFecho;
-    }
-
-    public String getTipoFecho() {
-        return this.tipoFecho;
-    }
-}
->>>>>>> 245278a6cd8eff9829fced3a782e4403c4f7faf6
