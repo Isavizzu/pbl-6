29ffb03b759e5ffd2c75bef26362418a359dde55
HelenaAlmeid <165021004+HelenaAlmeid@users.noreply.github.com>
2024-04-11 18:03:04 -0300

    .

--- e-commerce/src/Tenis.java
+++ e-commerce/src/Tenis.java
@@ -2,10 +2,19 @@ public class Tenis extends Calcado{
     private boolean cadarco;
     private float tamanhoPlataforma;
 
-    public Roupa(String material, String cor, String marca, String preco, String forma, String genero, int estoque, String nome, int numero, boolean cadarco, float tamanhoPlataforma){
-        super(material, cor, marca, preco, forma, genero, estoque, nome, numero);
+    public Roupa(String material, String cor, String marca, String preco, String forma, String genero, int estoque, String tipoCalcado, int numero, boolean cadarco, float tamanhoPlataforma){
+        super(material, cor, marca, preco, forma, genero, estoque, tipoCalcado, numero);
         this.cadarco = cadarco;
         this.tamanhoPlataforma = tamanhoPlataforma;
+    }
+
+    public void estoque(){
+        if (cadarco == true ) {
+            System.out.println("Há " + setEstoque() + " calçados de " + setTipoCalcado() + ", com cardaço no estoque!");
+        }
+        else {
+            System.out.println("Há " + setEstoque() + " calçados de " + setTipoCalcado() + ", sem cardaço no estoque!");
+        }
     }
 
 }
