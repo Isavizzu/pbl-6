3985f1f1b473e1ab45f56591632b17f855ac5e49
HelenaAlmeid <165021004+HelenaAlmeid@users.noreply.github.com>
2024-04-11 17:51:30 -0300

    Update Calcado.java

--- e-commerce/src/Calcado.java
+++ e-commerce/src/Calcado.java
@@ -1,11 +1,16 @@
 public class Calcado extends Vestuario {
-    protected String nome;
+    protected String tipoCalcado;
     protected int numero;
 
-    public Calcado(String material, String cor, String marca, String preco, String forma, String genero, int estoque, String nome, int numero){
+    public Calcado(String material, String cor, String marca, String preco, String forma, String genero, int estoque, String tipoCalcado, int numero){
         super(material, cor, marca, preco, forma, genero. estoque);
-        this.nome = nome;
+        this.tipoCalcado = tipoCalcado;
         this.numero = numero;
     }
 
+
+    public String setTipoCalcado(){
+        return  tipoCalcado;
+    }
+    
 }
