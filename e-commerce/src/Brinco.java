c39d7791fd8dd4a2f4d24d3e7e61cc4c1e0c134d
Maria Fernanda <162508333+Mafezuda@users.noreply.github.com>
2024-04-11 18:33:57 -0300

    Add files via upload

---
+++ e-commerce/src/Brinco.java
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+public class Brinco extends Acessorio{
+    private String tipo_brinco;
+
+    private String tamanho;
+
+}
+=======
+public class Brinco extends Acessorio{
+    private String tipo_brinco;
+
+    private String tamanho;
+
+}
+>>>>>>> 245278a6cd8eff9829fced3a782e4403c4f7faf6
